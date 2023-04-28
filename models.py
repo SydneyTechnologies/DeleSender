b2f8be6918743b27119f46c476875ad4197cd70c
sydneyIdundun <sydney.idundun@gmail.com>
2023-04-28 08:22:31 +0400

    made changes to fix the bugs on dates

--- models.py
+++ models.py
@@ -3,6 +3,13 @@ from datetime import datetime
 from enum import Enum
 from uuid import uuid4, UUID
 
+
+def getTrackingId():
+    return str(uuid4())
+
+def getDate():
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
 class OrderState(str, Enum):
     ordered = "ordered"
     shipped = "shipped"
@@ -17,14 +24,15 @@ class CreateOrderModel(BaseModel):
 class UpdateOrderStatusModel(BaseModel):
     status: OrderState = Field(default=OrderState.ordered)
     update_message: str | None = Field(default=None)
-    delivered_date: str = Field(default=datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    delivered_date: str = Field(default_factory=getDate)
+
 
 class Order(BaseModel):
-    tracking_id: str = Field(default=str(uuid4()))
+    tracking_id: str = Field(default_factory=getTrackingId)
     owner_email: str 
     description: str | None 
     status: OrderState = Field(default=OrderState.ordered)
-    date: str = Field(default=datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    date: str = Field(default_factory=getDate)
     delivered_date: str | None = Field(default=None)
     order_history: list[str] = []
 
